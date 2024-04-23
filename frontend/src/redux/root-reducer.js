24c169061c989db89d19fb080952fe598e6f30a3
marcusfjr <marcusfjunior@gmail.com>
2024-04-23 20:15:04 -0300

    ta indo

--- frontend/src/redux/root-reducer.js
+++ frontend/src/redux/root-reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import userReducer from "./user/slice";
-
+import torneioReducer from "./torneios/slice"
 const rootReducer = combineReducers({
   user: userReducer,
+  torneio: torneioReducer
 });
 
 export default rootReducer;
