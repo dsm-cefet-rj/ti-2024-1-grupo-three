7415833ec9a3a76d1f7ca96aece7c1fcd35edc12
lucasccalmon <lucascarneirocalmon@gmail.com>
2024-08-18 14:39:45 -0300

    CRUD time (falta relação com convites)

--- backend/src/controllers/timeController.js
+++ backend/src/controllers/timeController.js
@@ -1,5 +1,73 @@
 import { Time } from "../models/Time.js";
 const timeController = {
-  nome: "oi",
+  create: async (req, res) => {
+    try {
+      const time = {
+        id: req.body.id,
+        nomeTime: req.body.nometime,
+        userIdDono: req.body.useriddono,
+        userId: req.body.userid,
+        //algo de convites
+      };
+
+      const response = await Time.create(time);
+      res.status(201).json({ response, msg: "Time criado com sucesso" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getAll: async (req, res) => {
+    try {
+      const times = await Time.find();
+      req.json(times);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const time = await Time.findById(id);
+      if (!time) {
+        res.status(404).json({ msg: "erro, não encontrado" });
+        return;
+      }
+      res.json(time);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = req.params.id;
+      const time = await Time.findById(id);
+      if (!time) {
+        res.status(404).json({ msg: "erro, não encontrado" });
+        return;
+      }
+
+      const deletedTime = await Time.findByIdAndDelete(id);
+
+      res.status(200).json({ deletedTime, msg: "time excluido" });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  update: async (req, res) => {
+    const id = req.params.id;
+    const time = {
+      id: req.body.id,
+      nomeTime: req.body.nometime,
+      userIdDono: req.body.useriddono,
+      userId: req.body.userid,
+      //algo de convites
+    };
+    const updatedTime = await Time.findByIdAndUpdate(id, time);
+    if (!updatedTime) {
+      res.status(404).json({ msg: "erro, não encontrado" });
+      return;
+    }
+    res.status(200).json({ time, msg: "serviço atualizado com sucesso" });
+  },
 };
 export default timeController;
--- backend/src/routes/times.js
+++ backend/src/routes/times.js
@@ -2,4 +2,17 @@ import express from "express";
 import timeController from "../controllers/timeController.js";
 
 const timeRoutes = express.Router();
+timeRoutes.route("/times").post((req, res) => timeController.create(req, res));
+
+timeRoutes.route("/times").get((req, res) => timeController.getAll(req, res));
+
+timeRoutes.route("/times/:id").get((req, res) => timeController.get(req, res));
+timeRoutes
+  .route("/times/:id")
+  .delete((req, res) => timeController.delete(req, res));
+
+timeRoutes
+  .route("/times/:id")
+  .put((req, res) => timeController.update(req, res));
+
 export default timeRoutes;
